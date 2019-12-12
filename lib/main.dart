5cc4eb5e982a8076777cd1d0e99137ddcc6d5c66
zoharSoul <hunji66@gmail.com>
2019-12-12 23:59:29 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,73 @@ void main() {
 class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Scaffold is a layout for the major Material Components.
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: Text('NGA开源版'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                tooltip: 'Navigation menu',
+                onPressed: null,
+              ),
+              title: Text('NGA开源版'),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: 'Search',
+                  onPressed: null,
+                ),
+              ],
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(text: '1'),
+                  Tab(text: '2'),
+                  Tab(text: '3'),
+                  Tab(text: '4')
+                ],
+              ),
+            ),
+            // body is the majority of the screen.
+            body: TabBarView(
+              children: <Widget>[
+                Center(child: Text('1')),
+                Center(child: Text('2')),
+                Center(child: Text('3')),
+                Center(child: Text('4')),
+              ],
+            )));
+  }
+}
+
+class MyTabController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('DefaultTabController示例'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: '热点',
+              ),
+              Tab(
+                text: '体育',
+              ),
+              Tab(
+                text: '科技',
+              ),
+            ],
           ),
-        ],
-      ),
-      // body is the majority of the screen.
-      body: Center(
-        child: Text('Hello, world!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: Icon(Icons.add),
-        onPressed: null,
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Center(child: Text('热点')),
+            Center(child: Text('体育')),
+            Center(child: Text('科技')),
+          ],
+        ),
       ),
     );
   }
